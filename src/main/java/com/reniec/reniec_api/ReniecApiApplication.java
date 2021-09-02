6504852b1e989e7c8a198121ce78b4b074235937
Renato Valer <valer9826@gmail.com>
2021-09-01 23:14:10 -0500

    Creacion de proyecto

---
+++ src/main/java/com/reniec/reniec_api/ReniecApiApplication.java
@@ -0,0 +1,13 @@
+package com.reniec.reniec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReniecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReniecApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reniec/reniec_api/ReniecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reniec.reniec_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReniecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
