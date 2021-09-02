9bb84a468344667473565cf6e4ac060647c1799c
Renato Valer <valer9826@gmail.com>
2021-09-01 23:24:43 -0500

    Props Persona Model

--- src/main/java/com/reniec/model/Persona.java
+++ src/main/java/com/reniec/model/Persona.java
@@ -1,5 +1,17 @@
 package com.reniec.model;
 
+import java.util.Date;
+
 public class Persona {
 
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String sexo;
+    private Date fechaNacimiento;
+    private String estadoCivil;
+    private String departamento;
+    private String provincia;
+    private String distrito;
+    private String direccion;
 }
