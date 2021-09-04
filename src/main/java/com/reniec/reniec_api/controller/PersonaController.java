9c24d4cdb39dfbbb69e136acfede4d38a97a0f34
Renato Valer <valer9826@gmail.com>
2021-09-03 19:31:18 -0500

    Se hizo comentario Get Mostrar todas personas

--- src/main/java/com/reniec/reniec_api/controller/PersonaController.java
+++ src/main/java/com/reniec/reniec_api/controller/PersonaController.java
@@ -44,10 +44,10 @@ public class PersonaController {
         personas.put(id, p);
     }
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Persona>> all() {
-        return new ResponseEntity<Map<String, Persona>>(personas, HttpStatus.OK);
-    }
+    // @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    // public ResponseEntity<Map<String, Persona>> all() {
+    // return new ResponseEntity<Map<String, Persona>>(personas, HttpStatus.OK);
+    // }
 
     @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> add(@RequestBody Persona p) {
