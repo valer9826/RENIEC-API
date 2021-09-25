9a5dcea67690279cd3a330e81ce6437283ef68cd
Diego Linares <diegoalonsoll@hotmail.com>
2021-09-25 12:45:36 -0400

    correcciones

--- src/main/java/com/reniec/reniec_api/repository/TransaccionRepository.java
+++ src/main/java/com/reniec/reniec_api/repository/TransaccionRepository.java
@@ -1,7 +1,5 @@
 package com.reniec.reniec_api.repository;
 
-import com.reniec.reniec_api.model.Persona;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +7,7 @@ import java.util.Optional;
 import com.reniec.reniec_api.model.Transaccion;
 
 @Repository
-public interface TransaccionRepository extends JpaRepository<Persona, String> {
+public interface TransaccionRepository extends JpaRepository<Transaccion, Integer> {
     @Query(value = "SELECT * FROM Persona p where p.dni=?1 ")
-    Optional<Persona> findItemsByTransaccion(Transaccion transaccion);
+    Optional<Transaccion> findItemsByTransaccion(String dni);
 }
