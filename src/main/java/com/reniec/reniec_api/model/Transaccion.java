b16f6db3a8c9052930bc50f0311ed9759ecd82e7
Renato Valer <valer9826@gmail.com>
2021-09-25 16:43:09 -0500

    Transaccion ID

--- src/main/java/com/reniec/reniec_api/model/Transaccion.java
+++ src/main/java/com/reniec/reniec_api/model/Transaccion.java
@@ -17,7 +17,7 @@ public class Transaccion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id_Transaccion;
+    private int id;
     private Persona dni;
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date fecha_Transaccion;
