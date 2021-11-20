0ce4f14ff5eba79a384d8b4f37b5901798a57f0a
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-11-19 22:10:40 -0500

    Creo que funciona?

--- reniec_api/src/main/java/com/reniec/reniec_api/repository/TransaccionRepository.java
+++ reniec_api/src/main/java/com/reniec/reniec_api/repository/TransaccionRepository.java
@@ -2,14 +2,14 @@ package com.reniec.reniec_api.repository;
 
 import java.util.Optional;
 
-import com.reniec.reniec_api.model.Persona;
 import com.reniec.reniec_api.model.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransaccionRepository extends JpaRepository<Transaccion, String>{
-    @Query(value = "insert into t_transaccion values((select count(t_transaccion)+1 from t_transaccion), CURRENT_TIMESTAMP, persona_dni, 'Usmp@12')", nativeQuery = true)
-    Optional<Transaccion> transactQuery(Persona dni);
+    @Query(value = "insert into t_transaccion values((select count(t_transaccion)+1 from t_transaccion), CURRENT_TIMESTAMP, :dni, 'Usmp@12')", nativeQuery = true)
+    Optional<Transaccion> transactQuery(@Param("dni")String dni);
 }
